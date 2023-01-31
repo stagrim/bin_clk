6021255a1adbce3cf6778ceb004748ea56b7ad86
Esbjörn Stenberg <me@stagrim.com>
2023-01-31 20:00:11 +0100

    Initial commit

---
+++ _app/immutable/chunks/0-09befdda.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/layout.svelte-33a2617f.js";export{t as component};
---
+++ _app/immutable/chunks/1-aec2b2fd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-05bd3786.js";export{t as component};
