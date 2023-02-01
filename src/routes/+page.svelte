92f77f517f9d329d413a4557d6e77e9cf48bff35
Esbjörn Stenberg <me@stagrim.com>
2023-02-01 08:13:59 +0100

    Added footer, made more responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,21 +37,33 @@
     {/each}
 </div>
 
-<style>
+<p class="footer">
+    Made with <svg viewBox="0 0 1792 1792" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" style="height: 0.8rem;"><path d="M896 1664q-26 0-44-18l-624-602q-10-8-27.5-26T145 952.5 77 855 23.5 734 0 596q0-220 127-344t351-124q62 0 126.5 21.5t120 58T820 276t76 68q36-36 76-68t95.5-68.5 120-58T1314 128q224 0 351 124t127 344q0 221-229 450l-623 600q-18 18-44 18z" fill="#f280a1"></path></svg> by RootM
+</p>
+    
+<style lang="scss">
+    $diameter: 16vw;
+    $diameter-height-breakpoint: 26vh;
+
+    * {
+        font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+    }
+
     .container {
         display: flex;
         flex-direction: column;
         align-items: center;
+        margin-top: 2%;
     }
 
     .row {
         display: flex;
     }
-
     .circle {
-        width: 10vw;
-        height: 10vw;
-        margin: 4px;
+        width: min($diameter, $diameter-height-breakpoint);
+        height: $diameter;
+        max-height: $diameter-height-breakpoint;
+        margin: 5px;
         background-color: #2e2e2e;
         border-radius: 50%;
         transition: 0.1s;
@@ -62,6 +74,14 @@
     }
 
     .circle.active {
-        background-color: #F280A1;
+        background-color: #f280a1;
+    }
+
+    .footer {
+        bottom: 0;
+        width: 100%;
+        position: fixed;
+        text-align: center;
+        color: #787878;
     }
 </style>
